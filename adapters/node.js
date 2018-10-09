80f7c9cfd70d74517b2b0525f5546ad6519ffa24
sbdonslaught <littleman0507@hotmail.com>
2018-10-09 19:11:16 -0400

    Update node.js

--- adapters/node.js
+++ adapters/node.js
@@ -1,3 +1,10 @@
 module.exports = function() {
   try {throw new Error('Connection successful!');} catch(e) { throw new Error e.message }
 }
+//Missing a few if and else statements... I fixed
+if (hour < 18) {
+    greeting = "Good day";
+  else {
+  goodbye = "See ya later nerd";
+  }
+}
