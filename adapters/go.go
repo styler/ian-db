3b1aeabbcdd2ddbc7403c4b0f55ea138f94aa082
sbdonslaught <littleman0507@hotmail.com>
2018-10-09 19:07:48 -0400

    Update go.go

--- adapters/go.go
+++ adapters/go.go
@@ -1,1 +1,13 @@
 // I dont actually know go, can someone write this for me? Thanks.
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    fmt.Println("Hello", os.Args[1])
+    os.Exit(42)
+}
+//Got you CBenni
