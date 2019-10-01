74cda73e298752fec3cee1bdb2eba9dcbf806176
sbdonslaught <littleman0507@hotmail.com>
2019-10-01 17:09:19 -0400

    Learn to Delete Files

--- adapters/go.php
+++ adapters/go.php
@@ -1,5 +1,20 @@
-package main
+<?php
+$file = 'file_to_delete.txt';
 
-func main() {
-  fmt.Println(":)")
+// set up basic ftp connection
+$conn_id = ftp_connect($my_ftp_server);
+
+// login with my username and password
+ftp_login($conn_id, $my_ftp_user, $my_ftp_user);
+
+// try to delete $file
+if (ftp_delete($conn_id, $file)) {
+echo "$file deleted successful\n";
+} else {
+echo "could not delete $file\n";
 }
+
+// close the ftp connection
+ftp_close($conn_id);
+
+?>
